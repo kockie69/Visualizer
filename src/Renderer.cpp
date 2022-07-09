43b67e85ec5a0d58585e22f0223346c6a0599ee1
kockie69 <robert.kock@gmail.com>
2022-07-09 13:37:59 +0100

    Window hidden

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -401,7 +401,7 @@ GLFWwindow* TextureRenderer::createWindow() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  glfwWindowHint(GLFW_VISIBLE, GLFW_TRUE);
+  glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
   GLFWwindow* c = glfwCreateWindow(370, 400, "", NULL, APP->window->win);
   if (!c) {
     //rack::logger::log(rack::DEBUG_LEVEL, "LFM/" __FILE__, __LINE__, "LFM renderLoop could not create a context, bailing.");
