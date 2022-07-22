91dd78cf17fb9ed752d3e3356472225f3593b25d
kockie69 <robert.kock@gmail.com>
2022-07-22 17:29:16 +0100

    Additional HINT for Mac

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -351,7 +351,13 @@ GLFWwindow* WindowedRenderer::createWindow() {
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  
+  #if defined ARCH_MAC
+	glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_TRUE);
+  #endif
+
   GLFWwindow* c = glfwCreateWindow(RENDER_WIDTH, RACK_GRID_HEIGHT, "", NULL, NULL);  
+  
   if (!c) {
     return nullptr;
   }
@@ -416,6 +422,11 @@ GLFWwindow* TextureRenderer::createWindow() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
   glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+
+  #if defined ARCH_MAC
+	glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GLFW_TRUE);
+  #endif
+  
   GLFWwindow* c = glfwCreateWindow(RENDER_WIDTH, RACK_GRID_HEIGHT, "", NULL, NULL);
 
   if (!c) {
