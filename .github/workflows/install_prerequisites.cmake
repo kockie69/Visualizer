ad476d4bf62dc4d7a5d047c0aad00504f62f00d3
kockie69 <robert.kock@gmail.com>
2022-08-04 07:34:36 +0100

    Check result

--- .github/workflows/install_prerequisites.cmake
+++ .github/workflows/install_prerequisites.cmake
@@ -3,8 +3,9 @@ message(STATUS "Using host CMake version: ${CMAKE_VERSION}")
 if("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "Windows")
     # On Windows, using vcpkg to install and build is the best practice.
     set(VCPKG "$ENV{VCPKG_INSTALLATION_ROOT}/vcpkg.exe")
-    execute_process(COMMAND "${VCPKG}" --triplet=x64-windows install glew sdl2)
-    set(result 0)
+    execute_process(COMMAND "${VCPKG}" --triplet=x64-windows install glew sdl2
+    RESULT_VARIABLE result
+    )
 elseif("${CMAKE_HOST_SYSTEM_NAME}" STREQUAL "Linux")
     # On Ubuntu, installing the required dev packages is sufficient
     message(STATUS "Updating apt package sources")
